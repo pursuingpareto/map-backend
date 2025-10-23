ceadbe3131be27aba8c892fcbed4a701144619d5
Robbie Gleichman <rgleichman@gmail.com>
2025-10-23 11:38:01 -0700

    Make the map big

--- lib/storymap_web/components/layouts.ex
+++ lib/storymap_web/components/layouts.ex
@@ -62,8 +62,8 @@ defmodule StorymapWeb.Layouts do
       </div>
     </header>
 
-    <main class="px-4 py-20 sm:px-6 lg:px-8">
-      <div class="mx-auto max-w-2xl space-y-4">
+    <main>
+      <div>
         {render_slot(@inner_block)}
       </div>
     </main>
