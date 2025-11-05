21165f37bf6c5bd3dbe005f3d7aaca40d20c6a1e
Robbie Gleichman <rgleichman@gmail.com>
2025-11-05 11:53:14 -0800

    Run database migrations in build.sh

--- build.sh
+++ build.sh
@@ -15,6 +15,9 @@ MIX_ENV=prod mix assets.build
 # Build minified assets
 MIX_ENV=prod mix assets.deploy
 
+# Run database migrations
+MIX_ENV=prod mix ecto.migrate
+
 # Create server script, Build the release, and overwrite the existing release directory
 MIX_ENV=prod mix phx.gen.release
 MIX_ENV=prod mix release --overwrite
